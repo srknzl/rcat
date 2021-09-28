ad599293f0046018d16cddfc8db65982484b422a
Serkan Özel <serkanozel380@gmail.com>
2021-09-28 20:11:36 +0300

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::{env, process, fs};
+
 fn main() {
-    for i in 1..100 {
-        println!("{}", i);
+    let args : Vec<String> = env::args().skip(1).collect();
+
+    let number_of_args = args.len();
+
+    if number_of_args != 1 {
+        println!("Expected one argument, given {}", args.len());
+        process::exit(1);
+    }
+
+    let file_name = &args[0];
+
+    match fs::read_to_string(file_name) {
+        Ok(content) => {
+            print!("{}", content);
+        },
+        Err(err) => {
+            println!("Could not read file {}", err);
+            process::exit(1);
+        }
     }
 }
