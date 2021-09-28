413803c7293a22895be021b8cbd711e0514470a7
Serkan Özel <serkanozel380@gmail.com>
2021-09-28 20:41:03 +0300

    Add echo mode

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
-use std::{env, process, fs};
+use std::{env, process, fs, io};
 
 fn main() {
     let args : Vec<String> = env::args().skip(1).collect();
 
     let number_of_args = args.len();
 
-    if number_of_args != 1 {
-        println!("Expected one argument, given {}", args.len());
-        process::exit(1);
-    }
+    match number_of_args {
+        1 => {
+            let file_name = &args[0];
 
-    let file_name = &args[0];
-
-    match fs::read_to_string(file_name) {
-        Ok(content) => {
-            print!("{}", content);
+            match fs::read_to_string(file_name) {
+                Ok(content) => {
+                    print!("{}", content);
+                },
+                Err(err) => {
+                    println!("Could not read file {}", err);
+                    process::exit(1);
+                }
+            }
         },
-        Err(err) => {
-            println!("Could not read file {}", err);
+        0 => {
+            loop {
+                let mut str: String = String::new();
+                match io::stdin().read_line(&mut str) {
+                    Ok(_) => {},
+                    Err(err) => {
+                        println!("Cannot read input {}", err);
+                        process::exit(1);
+                    }
+                }
+                print!("{}", str);
+            }
+        },
+        _ => {
+            println!("Expected one argument, given {}", args.len());
             process::exit(1);
         }
     }
